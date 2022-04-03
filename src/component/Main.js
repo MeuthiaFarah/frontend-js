bc1fd9260f861f2149df758ab67648e7a0794434
MeuthiaFarah <tiasaloer@gmail.com>
2022-04-04 02:00:29 +0700

    upload ulang task-5

---
+++ src/component/Main.js
@@ -0,0 +1,19 @@
+import Hello from "./Hello";
+
+/**
+ * Membuat komponen main
+ * Menampung konten utama
+ */
+ function Main() {
+  return (
+    <main>
+      <Hello name="Meuthia" />
+      <Hello name="Meuthia lagi" />
+      <Hello name="Meuthia terus" />
+      <Hello name="Meuthia dulu"/>
+      <Hello name="Meuthia yang terakhir" />
+    </main>
+  );
+}
+
+export default Main;
